a0dccdd9f021e105ed877dda849937505bdd79d4
wliang002 <wliang002@berkeley.edu>
2020-05-29 17:39:39 -0700

    home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, FunctionComponent } from "react";
+
+
+const Home: FunctionComponent = props => {
+    return (
+        <div>
+            <div>
+                <h3>Hi there! Welcome to your education showcase</h3>
+            </div>
+            <div>
+                <h3>
+                    Type your name and click "Enter" below to begin!
+                </h3>
+                <input
+                >
+                </input>
+            </div>
+            <button>Enter</button>
+
+        </div>
+    )
+}
+
+export default Home
